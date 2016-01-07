bf02ed20296c515900bdcd2150ae7622394f1634
Andriy Oblivantsev <eslider@gmail.com>
2016-01-07 16:22:10 +0100

    Improve AppKernel to load Mapbender bundles

--- application/app/AppKernel.php
+++ application/app/AppKernel.php
@@ -5,8 +5,31 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
+    /**
+     * Search initialize and add Mapbender bundles.
+     * Search approach uses indirectly the composer auto generated file to get bundle names.
+     * It's good idea to use puli(http://docs.puli.io/) instead for the next release.
+     *
+     * @param array $bundles
+     * @return array
+     */
+    function addMapbenderBundles(array &$bundles)
+    {
+        $namespaces = include(dirname(__FILE__) . "/../vendor/composer/autoload_namespaces.php");
+        foreach ($namespaces as $name => $path) {
+            $bundleClassName = $name . '\\' . str_replace('\\', "", $name);
+            if (strpos($name, 'Mapbender\\') === 0) {
+                $bundles[] = new $bundleClassName();
+            }
+
+        }
+        return $bundles;
+    }
+
     public function registerBundles()
     {
+
+
         $bundles = array(
             // Standard Symfony2 bundles
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
@@ -35,13 +58,15 @@ class AppKernel extends Kernel
             new Mapbender\WmsBundle\MapbenderWmsBundle(),
             new Mapbender\ManagerBundle\MapbenderManagerBundle(),
             new Mapbender\PrintBundle\MapbenderPrintBundle(),
-            new Mapbender\DigitizerBundle\MapbenderDigitizerBundle(),
             new Mapbender\MobileBundle\MapbenderMobileBundle(),
 
             // OWSProxy3 bundles
             new OwsProxy3\CoreBundle\OwsProxy3CoreBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
+
+        $this->addMapbenderBundles($bundles);
+
 
         // dev and ALL test environments get some extra sugar...
         $isDevKernel = false;
